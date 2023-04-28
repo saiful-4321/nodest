1804ab6cf7938bbc5a0483b37da129c102049841
saif-paywell <bd.saif321@gmail.com>
2023-04-28 22:14:49 +0600

    Learned events

---
+++ Events/app.js
@@ -0,0 +1,12 @@
+const EventEmitter = require('events')
+
+const emitter = new EventEmitter()
+
+// Register a listener
+emitter.on('messageLogged', function() {
+    console.log('Listener called')
+})
+
+// Raise an event
+emitter.emit('messageLogged')
+
