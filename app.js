44a09012e286f4c9b9c6c2ec02464563afe0abcd
saif-paywell <bd.saif321@gmail.com>
2023-04-28 22:05:47 +0600

    learned os module

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 const logger    = require('./logger')
 const path      = require('path')
+const os        = require('os')
 
 logger("Hello message")
 // logger.log("Message")
 
+// Path Object
 var pathObj = path.parse(__filename)
-
 console.log(pathObj)
+
+// OS Module example
+let totalMemory = os.totalmem()
+let freeMemory = os.freemem()
+console.log(`Total Memory: ${totalMemory}`)
+console.log(`Free Memory: ${freeMemory}`)
